77781107343db5ac5b6a458cec91f87bbebe77d0
rt-saziracav <pavacarizas@gmail.com>
2018-06-18 10:27:20 +0800

    user model and view

--- spectrumone/appusers/models.py
+++ spectrumone/appusers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    email = models.CharField(max_length=100,)
+    password = models.CharField(max_length=100,)
+    firstname = models.CharField(max_length=100, blank=True, default='')
+    lastname = models.CharField(max_length=100, blank=True, default='')
+    activated = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('created',)
