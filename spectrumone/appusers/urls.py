4734001c78aeea96194c67c0c27352af1b9ef663
rt-saziracav <pavacarizas@gmail.com>
2018-06-18 10:56:39 +0800

    moved user registration call to "user" url

--- spectrumone/appusers/urls.py
+++ spectrumone/appusers/urls.py
@@ -3,5 +3,6 @@ from appusers import views
 
 urlpatterns = [
     url(r'^users/$', views.user_list),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^user/$', views.user_registration),
+    url(r'^user/(?P<pk>[0-9]+)/$', views.user_detail),
 ]
--- spectrumone/appusers/views.py
+++ spectrumone/appusers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -15,14 +15,20 @@ def user_list(request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
+    else:
+        return HttpResponseBadRequest("Not allowed")
 
-    elif request.method == 'POST':
+@csrf_exempt
+def user_registration(request):
+    if request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+    else:
+        return HttpResponseBadRequest("Not allowed")
 
 @csrf_exempt
 def user_detail(request, pk):
