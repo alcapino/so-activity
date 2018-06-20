48a0f17afd9a28bd0837aff928dedf7b43bf9ecb
rt-saziracav <pavacarizas@gmail.com>
2018-06-20 19:22:08 +0800

    add activation_token field for user

--- spectrumone/appusers/models.py
+++ spectrumone/appusers/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+def generateToken():
+    import random
+    return ''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(30))
 
 class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -9,6 +12,7 @@ class User(models.Model):
     firstname = models.CharField(max_length=100, blank=True, default='')
     lastname = models.CharField(max_length=100, blank=True, default='')
     active = models.BooleanField(default=False)
+    activation_token = models.CharField(max_length=30, default=generateToken)
 
     class Meta:
         ordering = ('created',)
--- spectrumone/appusers/serializers.py
+++ spectrumone/appusers/serializers.py
@@ -5,7 +5,7 @@ from appusers.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'firstname', 'lastname', 'active')
+        fields = ('id', 'email', 'password', 'firstname', 'lastname', 'active', 'activation_token')
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
@@ -16,5 +16,6 @@ class UserSerializer(serializers.ModelSerializer):
         instance.firstname = validated_data.get('firstname', instance.firstname)
         instance.lastname = validated_data.get('lastname', instance.lastname)
         instance.active = validated_data.get('active', instance.active)
+        instance.activation_token = validated_data.get('activation_token', instance.activation_token)
         instance.save()
         return instance
