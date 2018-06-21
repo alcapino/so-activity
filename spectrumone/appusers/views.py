967dcb0dcce3f9fc9d6b992d6803f72c3eaedf5a
rt-saziracav <pavacarizas@gmail.com>
2018-06-21 17:38:14 +0800

    use sendmail for sending emails

--- spectrumone/appusers/views.py
+++ spectrumone/appusers/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -6,18 +6,18 @@ from appusers.models import User
 from appusers.serializers import UserSerializer
 
 from rest_framework.decorators import api_view
+from django.core.mail import send_mail
 
 # Create your views here.
 
 #@csrf_exempt
 @api_view(['GET'])
 def user_list(request):
+    # TODO: if no token, omit email and lastname
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
-    else:
-        return HttpResponseBadRequest("Not allowed")
 
 @csrf_exempt
 @api_view(['POST'])
@@ -27,24 +27,31 @@ def user_registration(request):
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
+            send_mail(
+                #TODO: email not working, URL not restful
+                "User activation",
+                ("Use this link to activate: http://127.0.0.1:8000/activate/%s/%s", str(serializer.data['id']), str(serializer.data['activation_token'])),
+                "admin@so.com",
+                [str(serializer.data['email'])],
+                fail_silently=False,
+            )
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-    else:
-        return HttpResponseBadRequest("Not allowed")
 
 #@csrf_exempt
 @api_view(['GET','PUT','DELETE'])
-def user_detail(request, pk):          # TODO: change responses, add messages
+def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse("User not found", status=404)
 
     if request.method == 'GET':
         serializer = UserSerializer(user)
         return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
+        # TODO: require token and current password
         data = {}
         data["email"] = user.email
         data["password"] = user.password
@@ -61,7 +68,4 @@ def user_detail(request, pk):          # TODO: change responses, add messages
 
     elif request.method == 'DELETE':
         user.delete()
-        return HttpResponse(status=204)
-
-    else:
-        return HttpResponseBadRequest("Not allowed")
+        return HttpResponse("User removed", status=204)
